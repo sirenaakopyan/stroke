e1425f03ff4c26ed14a517c338db0e735782c8a3
krendu <krendu@uw.edu>
2023-03-02 17:20:54 -0800

    made into separate modules

---
+++ code/dataanalysis.py
@@ -0,0 +1,30 @@
+import datacleanup
+import pandas as pd
+
+
+def find_risk_factor_correlation(risk_factor_df: pd.DataFrame) -> float:
+    '''Calculate the correlation matrix.
+    Sort the correlation coefficients in descending order.
+    Return the sorted correlation coefficients.
+    '''
+    corr_matrix = risk_factor_df.corr()
+    stroke_corr = corr_matrix["stroke"]
+    stroke_corr_sorted = stroke_corr.abs().sort_values(ascending=False)
+    return stroke_corr_sorted
+
+
+def main():
+    risk_factor_data = datacleanup.create_risk_factor_df(
+        'datasets/stroke_data_1.csv')
+    map_data = datacleanup.create_shapefile_for_bubble_map(
+        "datasets/tl_2017_us_state/tl_2017_us_state.shp",
+        "datasets/hypertension_by_state.xlsx",
+        "datasets/Obesity_by_state.csv",
+        "datasets/Diabetes_by_state.csv",
+        "datasets/State_code_to_name.csv",
+        "datasets/stroke_mortality_state.csv")              
+    print(find_risk_factor_correlation(risk_factor_data))
+
+
+if __name__ == '__main__':
+    main()
