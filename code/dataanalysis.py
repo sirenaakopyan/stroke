2ebb7b848b14dbb09c8606b02d059f80e040f5de
Sirena Akopyan <sirenaakopyan@gmail.com>
2023-03-06 13:27:14 -0800

    working on bubble map

--- code/dataanalysis.py
+++ code/dataanalysis.py
@@ -1,7 +1,7 @@
 import datacleanup
 import pandas as pd
 import numpy as np
-
+import geopandas as gpd
 
 def find_risk_factor_correlation(risk_factor_df: pd.DataFrame) -> float:
     '''Calculate the correlation matrix.
@@ -47,6 +47,39 @@ def risk_factor_df_ML(dataframe: str) -> pd.DataFrame:
     mL_df = pd.merge(dataframe, dataframe3, left_index=True, right_index=True)
     return mL_df
     
+def map_risk_factors(dataframe: str):
+    """
+    Display a bubble map of the top risk factors 
+    across the country.
+    """
+    # # load the state codes and names
+    # state_codes = pd.read_csv("datasets/State_code_to_name.csv")
+
+    # # merge the data with the state codes
+    # merged_data = pd.merge(dataframe, state_codes, on="state_code")
+
+    # # create a scatter_geo trace for hypertension by state
+    # fig = px.scatter_geo(merged_data, locations="state_code",
+    #                      locationmode="USA-states", color="state_name",
+    #                      size="hypertension", hover_name="state_name",
+    #                      hover_data=["hypertension"])
+
+    # # load the stroke mortality data by state
+    # stroke_data = pd.read_csv("datasets/stroke_mortality_state.csv")
+    # # merge with the state codes
+    # merged_stroke_data = pd.merge(stroke_data, state_codes, on="state_code")
+
+    # # add a choropleth map of stroke mortality by state
+    # fig.add_choropleth(locations=merged_stroke_data["state_code"],
+    #                    z=merged_stroke_data["stroke_mortality"],
+    #                    locationmode="USA-states",
+    #                    colorscale="Reds", colorbar_title="Stroke Mortality",
+    #                    geojson=counties_json)
+
+    # fig.update_layout(title="Hypertension and Stroke Mortality by State",
+    #                   geo_scope="usa")
+    pass
+
 
 def main():
     risk_factor_data = datacleanup.create_risk_factor_df(
@@ -57,10 +90,12 @@ def main():
         "datasets/Obesity_by_state.csv",
         "datasets/Diabetes_by_state.csv",
         "datasets/State_code_to_name.csv",
-        "datasets/stroke_mortality_state.csv")            
+        "datasets/stroke_mortality_state.csv")         
+    us_map = gpd.read_file("datasets/tl_2017_us_state/tl_2017_us_state.shp")
+    print(us_map.columns)
     print(find_risk_factor_correlation(risk_factor_data))
     print(risk_factor_df_ML(risk_factor_data))
-    
+    map_risk_factors(map_data)
 
 
 
