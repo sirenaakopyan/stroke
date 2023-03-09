53d2ad8d00020a0d708273d82bf4516c6d227b88
krendu <krendu@uw.edu>
2023-03-09 11:59:21 -0800

    ?

--- code/datacleanup.py
+++ code/datacleanup.py
@@ -9,68 +9,6 @@ import geopandas as gpd
 from functools import reduce
 import dataanalysis
 
-unclean_data = pd.read_csv('datasets/stroke_data_1.csv')
-
-'''
-cleaning data in gender column. Male = 1 and Female = 0
-we do not worry about other genders because they are not
-included in the dataset
-'''
-cleaned_gender = unclean_data.copy()
-cleaned_gender.loc[cleaned_gender["gender"] == "Male", "gender"] = 1
-cleaned_gender.loc[cleaned_gender["gender"] == "Female", "gender"] = 0
-'''cleaning data in smoking_status column. has smoked = 1.
-Never smoked = 0. UNSURE WHAT TO DO ABOUT UNKNOWN'''
-cleaned_smoking = cleaned_gender.copy()
-cleaned_smoking.loc[cleaned_smoking['smoking_status'] == 'formerly smoked',
-                    'smoking_status'] = 1
-cleaned_smoking.loc[cleaned_smoking['smoking_status'] == 'smokes',
-                    'smoking_status'] = 1
-cleaned_smoking.loc[cleaned_smoking['smoking_status'] == 'never smoked',
-                    'smoking_status'] = 0
-print(cleaned_smoking)
-''' for BMI, according to the CDC
-If your BMI is less than 18.5, it falls within the underweight range.
-If your BMI is 18.5 to <25, it falls within the healthy weight range.
-If your BMI is 25.0 to <30, it falls within the overweight range.
-If your BMI is 30.0 or higher, it falls within the obesity range.
-'''
-# for now, convert BMI into categories
-# then determine if we can convert to binary
-cleaned_BMI = cleaned_smoking.copy()
-cleaned_BMI['BMI_cat'] = np.where(cleaned_BMI['bmi'] < 18.5,
-                                  'underweight',
-                                  np.where((cleaned_BMI['bmi'] >= 18.5) & (cleaned_BMI['bmi'] <= 24.9),
-                                           'healthy weight',
-                                           np.where((cleaned_BMI['bmi'] >= 25) & (cleaned_BMI['bmi'] <= 29.9),
-                                                    'overweight',
-                                                    'obese')))
-
-
-#print(cleaned_BMI)
-
-# convert avg_glucose_level into binary
-# 99 or below is 0, 100+ is 1
-# source: cdc gov
-
-cleaned_glucose = cleaned_BMI.copy()
-cleaned_glucose['cleaned_avg_glucose'] = np.where((cleaned_glucose['avg_glucose_level'] <= 99),
-                                                  0, np.where((cleaned_glucose['avg_glucose_level'] >= 100),
-                                                              1, cleaned_glucose['avg_glucose_level'])).astype(int)
-
-#print(cleaned_glucose)
-
-# convert age into binary
-# less than 65 is 0
-# 65+ is 1
-
-cleaned_age = cleaned_glucose.copy()
-cleaned_age['cleaned_age'] = np.where((cleaned_age['age'] <= 64),
-                                              0, np.where((cleaned_age['age'] >= 65),
-                                                          1, cleaned_age['age'])).astype(int)
-print(cleaned_age)
-
-
 def create_risk_factor_df(stroke_data: str) -> pd.DataFrame:
     '''
     Cleans the stroke data file. Converts all columns of interest to binary.
@@ -186,19 +124,6 @@ def create_shapefile_for_bubble_map(shapefile: str, hypertension: str, obesity: 
     risk_factors_and_stroke_df = us_shapefile.merge(merged_risk_factors, left_on='State',
                                                     right_on='State', how='inner')
     return risk_factors_and_stroke_df
-
-
-# Calculate the correlation matrix
-corr_matrix = cleaned_age.corr()
-
-# Extract the correlation coefficients for stroke
-stroke_corr = corr_matrix["stroke"]
-
-# Sort the correlation coefficients in descending order
-stroke_corr_sorted = stroke_corr.abs().sort_values(ascending=False)
-
-# Print the sorted correlation coefficients
-print(stroke_corr_sorted)
 
 def create_risk_factor_df(stroke_data: str) -> pd.DataFrame:
     '''
@@ -338,7 +263,6 @@ def create_shapefile_for_bubble_map(shapefile: str, hypertension: str, obesity: 
     # print("/n=======================================================")
     # print(us_shapefile)
     # combine with shapefile
-<<<<<<< HEAD
     risk_factors_and_stroke_df = us_shapefile.merge(
         merged_risk_factors, left_on='State', right_on='State', how='inner')
     # print("/n=======================================================")
@@ -347,41 +271,5 @@ def create_shapefile_for_bubble_map(shapefile: str, hypertension: str, obesity: 
 
 
     return risk_factors_and_stroke_df
-=======
-    risk_factors_and_stroke_df = us_shapefile.merge(merged_risk_factors, left_on='State',
-                                    right_on='State', how='inner')
-    return risk_factors_and_stroke_df
 
 
-def risk_factor_df_ML(dataframe: str) -> pd.DataFrame:
-    correlations = dataanalysis.find_risk_factor_correlation(dataframe)
-    over_65 = correlations[1]
-    heart_disease = correlations[2]
-    hypertension = correlations[3]
-    married = correlations[4]
-    low_bmi = correlations[5]
-    residence = correlations[6]
-    high_BMI = correlations[7]
-    high_glucose = correlations[8]
-    dataframe2 = dataframe.copy()
-    dataframe2 = dataframe2[['hypertension', 'heart_disease', 'high_glucose','low_BMI',
-                             'high_BMI', 'over_65', 'married', 'residence']]
-    dataframe2["hypertension"] = np.where(dataframe2["hypertension"] == 1, hypertension, 0)
-    dataframe2['heart_disease'] = np.where(dataframe2['heart_disease'] == 1,
-                                           heart_disease, 0)
-    dataframe2['high_glucose'] = np.where(dataframe2['high_glucose'] == 1,
-                                          high_glucose, 0)
-    dataframe2['low_BMI'] = np.where(dataframe2['low_BMI'] == 1, low_bmi, 0)
-    dataframe2['high_BMI'] = np.where(dataframe2['high_BMI'] == 1, high_BMI,
-                                       0)
-    dataframe2['over_65'] = np.where(dataframe2['over_65'] == 1, over_65,
-                                     0)
-    dataframe2['married'] = np.where(dataframe2['married'] == 1, married,
-                                     0)
-    dataframe2['residence'] = np.where(dataframe2['residence'] == 1, residence,
-                                       0)
-    dataframe = dataframe[['stroke']]
-    mL_df = pd.merge(dataframe, dataframe2, left_index=True, right_index=True)
-    # mL_df = mL_df[['stroke', 'row_corr']]
-    return mL_df
->>>>>>> aad4861dc5976626f4cb5f957d2cd623f26eb234
