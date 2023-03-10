22a270b808cb8fdf2da57e2ba67f6a04eb983496
krendu <krendu@uw.edu>
2023-03-09 20:01:07 -0800

    changed how to calculate corr

--- code/ML_and_visualization.py
+++ code/ML_and_visualization.py
@@ -1,5 +1,6 @@
 import cleanup_ML
 import datacleanup
+import dataanalysis
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
@@ -8,16 +9,10 @@ from sklearn.model_selection import train_test_split
 
 
 def fit_and_predit_stroke(ml_df: pd.DataFrame) -> list:
-    # normalize data
-    shuffled_data = ml_df.sample(frac=1,random_state=4)
-    stroke_df = ml_df.loc[ml_df['stroke'] == 1]
-    non_stroke_df = ml_df.loc[ml_df['stroke'] == 0].sample(n=1000, random_state=101)
-    # non-stroke sufferers were reduced to 1000 to balance the data set.
-    normalized_stroke = pd.concat([stroke_df, non_stroke_df])
-    # features is the accuracy score
-    features = normalized_stroke.drop('stroke', axis=1)
+    # features is the risk_row_corr
+    features = ml_df.drop('stroke', axis=1)
     # label is stroke, which we want to predict
-    labels = normalized_stroke['stroke']
+    labels = ml_df['stroke']
     # Breaks the data into 80% train and 20% test
     features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2)
     logistic_model = LogisticRegression(solver='liblinear', random_state=101)
@@ -45,7 +40,8 @@ def plot_confusion_matrix(cm: list) -> None:
 def main():
     risk_factor_data = datacleanup.create_risk_factor_df(
         'datasets/stroke_data_1.csv')
-    ml_data = cleanup_ML.risk_factor_df_ML(risk_factor_data)
+    normalized_df = dataanalysis.normalize_data(risk_factor_data)
+    ml_data = cleanup_ML.risk_factor_df_ML(normalized_df)
     confusion_matrix = fit_and_predit_stroke(ml_data)
     print(confusion_matrix)
     print(f"True Negatives: {((((confusion_matrix[0])[0]))[0])},\
